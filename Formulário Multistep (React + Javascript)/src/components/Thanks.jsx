864b86a0a5450f3247e9296d18344b664683886d
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-11-20 10:21:01 -0300

    Components, Hooks and .jsx and .css Updates

---
+++ Formulário Multistep (React + Javascript)/src/components/Thanks.jsx
@@ -0,0 +1,46 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./Thanks.css";
+import PropTypes from 'prop-types';
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+};
+
+const Thanks = ({ data }) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de 10%
+        de desconto para a sua próxima compra.
+      </p>
+      <p>Para concluir sua avaliação clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span> {data.comment}
+      </p>
+    </div>
+  );
+};
+Thanks.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    review: PropTypes.oneOf(['unsatisfied', 'neutral', 'satisfied', 'very_satisfied']).isRequired,
+    comment: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Thanks;
