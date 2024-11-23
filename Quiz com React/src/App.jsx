7ae2bccf7356b8bbb8cdf9fdb4c437f34e3e8673
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-11-22 22:45:56 -0300

    Workspace Update

--- Quiz com React/src/App.jsx
+++ Quiz com React/src/App.jsx
@@ -1,17 +1,26 @@
-// react, componentesm, estáticos
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 
-import Welcome from './components/Welcome'
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
 
-import './App.css'
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Quiz com React/src/main.jsx
+++ Quiz com React/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import QuizProvider from './context/quiz.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QuizProvider } from "./context/quiz";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <QuizProvider>
       <App />
     </QuizProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
