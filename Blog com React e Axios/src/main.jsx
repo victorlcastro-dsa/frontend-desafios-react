1aa058021b9d29c3ced5c9dd4ae1342a704088f0
Victor Lopes de Castro <victorlcastro.dsa@gmail.com>
2024-11-26 14:27:42 -0300

    Basic Route Creation

--- Blog com React e Axios/src/main.jsx
+++ Blog com React e Axios/src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+
+// Páginas
+import Home from './routes/Home'
+import NewPost from './routes/NewPost'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ Blog com React e Axios/src/routes/NewPost.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewPost = () => {
+  return (
+    <div>NewPost</div>
+  )
+}
